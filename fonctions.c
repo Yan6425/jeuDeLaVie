7fb33acc7c9a2fe07241a2e7be0ce76da280d0c5
Yan <yan.hourdebaigt.hiroux@gmail.com>
2024-01-29 12:18:27 +0100

    fonction trouverVoisines

--- fonctions.c
+++ fonctions.c
@@ -6,20 +6,31 @@
 
 int placerCellule(Cellule* tblCellules, int nbCellules, int x, int y){
     tblCellules = realloc((nbCellules + 1) * sizeof(Cellule));
-    tblCellules[nbCellules].x = x;
-    tblCellules[nbCellules].y = y;
+    tblCellules[nbCellules]->x = x;
+    tblCellules[nbCellules]->y = y;
     nbCellules++;
-    trouverVoisins(tblCellules, nbCellules, iCellule);
+    trouverVoisines(tblCellules, nbCellules, iCellule);
     return nbCellules;
 }
 
 
-void trouverVoisins(Cellule* tblCellules, int nbCellules, int iCellule){
-    int x = tblCellules[iCellule].x;
-    int y = tblCellules[iCellule].y;
-    for (int i = 0; i < nbCellules, i++){
-        if (i = iCellule){break;}
-        Cellule cellule = tblCellules[i];
+void trouverVoisines(Cellule* tblCellules, int nbCellules, int iCellule){
+    int x = tblCellules[iCellule]->x;
+    int y = tblCellules[iCellule]->y;
+    tblCellules[iCellule]->voisines = malloc(3 * sizeof(*Cellule));
+    for (int = 0; i < 3; i++){
+        tblCellules[iCellule]->voisines[i] = malloc(3 * sizeof(Cellule));
+    }
+    for (int i = 0; i < nbCellules; i++){
+        for (int j = -1; j <= 1; j++){
+            for (int h = -1; h <=1; h++){
+                if (!j && !h){break;}
+                Cellule cellule = tblCellules[i];
+                if (cellule->x - x == j && cellule->y - y == h){
+                    tblCellules[iCellule]->voisines[j][h] = cellule; 
+                }
+            }
+        }
     }
 }
 
--- fonctions.h
+++ fonctions.h
@@ -2,14 +2,14 @@
 #define __fonctions_H_
 
 typedef struct cellule {
-    struct cellule **voisines;
+    struct cellule ***voisines;
     int x;
     int y;
 } *Cellule;
 
 
 int placerCellule(Cellule* tblCellules, int nbCellules, int x, int y);
-void trouverVoisins(Cellule* tblCellules, int nbCellules, int iCellule);
+void trouverVoisines(Cellule* tblCellules, int nbCellules, int iCellule);
 int deplacements(Cellule* tblCellules, int nbCellules);
 void afficher(Cellule* tblCellules, int nbCellules, int x, int y, int taille);
 int** initTbl(int taille);
